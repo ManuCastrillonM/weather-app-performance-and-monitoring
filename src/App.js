06ca2c50ed2f78d9d5c072a188ef44100f4b32f4
manuCastrillonM <10585946+ManuCastrillonM@users.noreply.github.com>
2025-06-13 22:56:55 +0200

    add user events

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import ActionButtons from './ActionButtons/ActionButtons';
 import WeatherSearch from './WeatherSearch/WeatherSearch';
 import WorldMap from './WorldMap/WorldMap';
 import WeatherCard from './WeatherCard/WeatherCard';
@@ -98,6 +99,7 @@ function App() {
 
   return (
     <div className="weather-app-container">
+      <ActionButtons />
       <h1 className="weather-title">Weather</h1>
       <WeatherSearch
         input={input}
